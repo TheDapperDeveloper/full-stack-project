a7d1aa299305fccf4abf8fba926a2d098864c5ac
Brande Gilbert <brandegilbert@gmail.com>
2021-09-20 16:40:47 -0400

    Figuring out foreign-keys

--- models/itinerary.js
+++ models/itinerary.js
@@ -10,10 +10,18 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      itinerary.belongsTo(models.userregistration, {
+        foreignKey: 'userID'
+      }),
+      itinerary.hasMany(models.trips, {
+        foreignKey:'tripID'
+      });
       // define association here
-    }
+    };
   };
   itinerary.init({
+    userID: DataTypes.INTEGER,
+    tripID: DataTypes.STRING,
     arrDate: DataTypes.DATEONLY,
     depDate: DataTypes.DATEONLY,
     location: DataTypes.STRING
--- models/userregistration.js
+++ models/userregistration.js
@@ -10,6 +10,9 @@ module.exports = (sequelize, DataTypes) => {
      * The `models/index` file will call this method automatically.
      */
     static associate(models) {
+      userRegistration.hasOne(models.itinerary, {
+        foreignKey: 'userID'
+      })
       // define association here
     }
   };
