dc374f69efaa9254372be714dac93bf07db4f874
JBama023 <jbama023@gmail.com>
2021-09-20 13:59:59 -0500

    changed registration

--- registration.js
+++ registration.js
@@ -1,32 +1,28 @@
 const submitButton = document.querySelector(".submitbutton")
 
 const createUser = async () => {
-   const url = ""
-   const name = document.querySelector(".name").value
-   const username = document.querySelector(".username").value
-   const password = document.querySelector(".password").value
-
-   const userData = {
-       name,
-       username,
-       password
-   };
-};
-
-const submitData = async () => {
-    const url = ""
-    const postUser = await fetch(url,{
-        method: "POST",
-        mode: "cors",
-        headers: {
-            "Content-type": "applicaiton/json"
-        },
-        body: JSON.stringify(userData)
+    const url = "";
+    const firstName = document.querySelector(".firstName").value;
+    const lastName = document.querySelector(".lastName").value;
+    const userName = document.querySelector(".userName").value;
+    const password = document.querySelector(".password").value
+  
+    const userData = {
+      firstName,
+      lastName,
+      userName,
+      password
+    };
+  
+    const createUser = await fetch(url, {
+      method: "POST",
+      mode: "cors",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify(userData),
     });
-};
-const json = await submitData.json()
-
-submitButton.addEventListener("click", () => {
+  };
+  submitButton.addEventListener("click", () => {
     createUser();
-});
-submitData();
+  });
