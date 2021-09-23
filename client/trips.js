6118558f1fefd5de0b80aa6b18ab752077caf2e0
Brande Gilbert <brandegilbert@gmail.com>
2021-09-23 12:02:50 -0400

    Got trips to display on trips page

--- client/trips.js
+++ client/trips.js
@@ -1,18 +1,61 @@
-const url = "";
+// const e = require("cors");
 
-const trips = document.querySelector(".div2")
+// const url = "";
 
-async function getTrips() {
-    const trips = await fetch(url);
-    const getTrip = await trips.json();
-    const tripImg = document.createElement("img")
-    const explanation = document.createElement("p")
-    getTripImg.src = getTrip.url
-    getTripImg.style.height = "300px"
-    getTripImg.style.width = "300px"
-    createDiv.innerHTML = tripImg.url
+
+const submitbutton = document.querySelector(".submitbutton")
+const tripsDiv = document.querySelector(".tripsdiv")
+
+const getTrips = async () => {
+    const url = "http://localhost:3000/view_trips";
+    const tripData = await fetch (url, {
+        method: "POST",
+        mode: "cors",
+        headers: {
+      "Content-Type": "application/json",
+    },
+  });
+
+  const json = await tripData.json();
+  for (const trip of json) {
+      const tripTitle = trip.location;
+      const displayTrip = document.createElement("h2")
+      displayTrip.innerHTML = tripTitle
+
+      const tripPhoto = trip.photo;
+      const displayPhoto = document.createElement("img")
+      displayPhoto.src = tripPhoto
+
+      const tripDes = trip.description
+      const displayDes = document.createElement("p")
+      displayDes.innerHTML = tripDes
+
+      const button 
+
+      const trips = document.createElement("div")
+
+      trips.append(displayTrip, displayPhoto, displayDes)
+      tripsDiv.append(trips)
+  }
+};
+
+
+// async function getTrips() {
+//     const trips = await fetch("http://localhost:3000/view_trips");
+//     const getTrip = await trips.json();
+//     console.log(getTrip)
+//     const tripImg = document.createElement("img")
+//     const explanation = document.createElement("p")
+//     tripImg.src = getTrip.url
+//     tripImg.style.height = "300px"
+//     tripImg.style.width = "300px"
+//     createDiv.innerHTML = tripImg.url
     
-trips.append(tripImg, explanation)
-}
+// trips.append(tripImg, explanation)
+// }
 
-getTrips();
+submitbutton.addEventListener("click", () => {
+    getTrips();
+  });
+
+ 
